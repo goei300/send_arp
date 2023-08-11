2fb50f4938b89fb8f88dda848d46fc0998343677
MONTH4 <goei4559@gmail.com>
2023-08-12 05:46:36 +0900

    arp_spoofing end & relay init

--- init.h
+++ init.h
@@ -1,5 +1,10 @@
 #pragma once
 #include <string>
 
-unsigned char* getMyMac(const char* dev);
-std::string getSenderMac(std::string sdr,const char* dev,unsigned char *mymac);
+std::string checkSend(const u_char* pac);
+std::string getMyIp(const char* dev);
+std::string getSenderMac(std::string sdr,const char* dev,std::string mymac);
+std::string getMyMac(const char* dev);
+bool sendArpSpoof(std::string sI,std::string tI,const char* dev,std::string sM,std::string mM);
+bool isBroadcast(const u_char *packet);
+void relay_thread(const u_char* pac,const char* dev,std::string tm);
